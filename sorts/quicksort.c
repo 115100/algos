18b0a2c15af767626b025a5028ba30157f0c30d4
115100 <115100@users.noreply.github.com>
2016-06-18 22:47:38 +0100

    Implementation of Hoare-partition quicksort

--- sorts/quicksort.c
+++ sorts/quicksort.c
@@ -3,46 +3,66 @@
 
 // Theoretical O(nLog(n)) average algorithm.
 
-
-void quick_sort(int unsortedList[], int listSize)
+int partition(int A[], int lo, int hi)
 {
-	int pivot, i, j, tmp;
+	int pivot;
 
-	if (listSize < 2)
+	int i = lo - 1;
+	int j = hi + 1;
+
+	// // Get median of three
+	int mid = j - (j - i) / 2;
+
+	// Arrange so that A[lo] <= A[mid] <= A[hi]
+	// Writing a swap function would probably increase
+	// function call overhead quite a bit?
+	int tmp;
+	if (A[lo] > A[hi])
 	{
-		return;
+		tmp = A[lo];
+		A[lo] = A[hi];
+		A[hi] = tmp;
+	}
+	if (A[lo] > A[mid])
+	{
+		tmp = A[lo];
+		A[lo] = A[mid];
+		A[mid] = tmp;
+	}
+	if (A[mid] > A[hi])
+	{
+		tmp = A[mid];
+		A[mid] = A[hi];
+		A[hi] = tmp;
 	}
 
-	// Choose pivot to be in the middle
-	pivot = unsortedList[listSize / 2];
+	pivot = A[mid];
 
-	for (i = 0, j = listSize - 1; 1; i++, j--)
+	while (1)
 	{
-
-		while (unsortedList[i] < pivot)
-		{
-			i++;
-		}
-
-		while (pivot < unsortedList[j])
-		{
-			j--;
-		}
+		do i++; while (A[i] < pivot);
+		do j--; while (A[j] > pivot);
 
 		if (i >= j)
-		{
-			break;
-		}
+			return j;
 
-		// Swap i and j index
-		tmp = unsortedList[j];
-		unsortedList[j] = unsortedList[i];
-		unsortedList[i] = tmp;
-
+		int tmp;
+		tmp = A[i];
+		A[i] = A[j];
+		A[j] = tmp;
 	}
 
-	quick_sort(unsortedList, i);
-	quick_sort(unsortedList + i, listSize - i);
+}
+
+
+void quicksort(int A[], int lo, int hi)
+{
+	if (hi - lo < 2) return;
+
+	int p = partition(A, lo, hi);
+
+	quicksort(A, lo, p);
+	quicksort(A, p + 1, hi);
 }
 
 
@@ -57,7 +77,7 @@ int main(int argc, char *argv[])
 		unsortedList[i] = atoi(argv[i + 1]);
 	}
 
-	quick_sort(unsortedList, listSize);
+	quicksort(unsortedList, 0, listSize - 1);
 
 	return 0;
 }
